783ea780d425f8b8ef0d19092bfcae95b8e0a82a
seyran7 <brsr888@gmail.com>
2026-07-06 00:25:30 +0400

    chore: initialize Spring Boot project

---
+++ src/test/java/com/seyran/scda/SmartCreditDecisionAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyran.scda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCreditDecisionAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
